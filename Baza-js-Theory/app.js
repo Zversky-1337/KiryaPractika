b9a7cdc3ec0c069a06f7bf02a5a2bfa32e143e5f
Zversky-1337 <poshevkin1687@gmail.com>
2025-06-15 18:13:28 +0500

    Дописал теорию

--- Baza-js-Theory/app.js
+++ Baza-js-Theory/app.js
@@ -232,21 +232,286 @@
 
 // console.log(city);
 
-const logUser = (user) => {
-  const { name, age, city, ...rest } = user;
+// const logUser = (user) => {
+//   const { name, age, city, ...rest } = user;
 
-  console.log(`
-        Имя: ${name}
-        Возраст: ${age}
-        Город: ${city}
-        `);
-  console.log(rest);
-};
+//   console.log(`
+//         Имя: ${name}
+//         Возраст: ${age}
+//         Город: ${city}
+//         `);
+//   console.log(rest);
+// };
 
-logUser({
-  name: "Александр",
-  age: 29,
-  city: "Москва",
-  company: "ВК",
-  jobPost: "Фронтенд-разработчик",
-});
+// logUser({
+//   name: "Александр",
+//   age: 29,
+//   city: "Москва",
+//   company: "ВК",
+//   jobPost: "Фронтенд-разработчик",
+// });
+
+// console.log("This global:", this);
+
+// function fn() {
+//   console.log("This body", this);
+// }
+
+// fn();
+
+// const user = {
+//   name: "Alex",
+//   age: 23,
+//   logThis() {
+//     console.log("This body metod", this);
+//     console.log(this.name);
+//   },
+//   address: {
+//     city: "Moscow",
+//     zipcode: 123131,
+//     logInnerThis: function () {
+//       console.log("This body metod address", this);
+//     },
+//   },
+// };
+
+// user.logThis();
+// user.address.logInnerThis();
+
+// const user1 = { name: "Max" };
+// const user2 = { name: "Alex" };
+
+// function logInfo() {
+//   console.log("This:", this);
+//   console.log("This name:", this.name);
+// }
+
+// logInfo();
+
+// user1.logName = logInfo;
+// user2.logName = logInfo;
+
+// user1.logName();
+// user2.logName();
+
+// const calculator = {
+//   read() {
+//     this.a = Number(prompt("Введите первое число", 0));
+//     this.b = Number(prompt("Введите второе число", 0));
+//   },
+//   summ() {
+//     return this.a + this.b;
+//   },
+//   mul() {
+//     return this.a * this.b;
+//   },
+// };
+
+// calculator.read();
+// console.log(calculator);
+// console.log(calculator.summ());
+// console.log(calculator.mul());
+
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//     return this;
+//   },
+//   down() {
+//     this.step--;
+//     return this;
+//   },
+//   showStep() {
+//     console.log(this.step);
+//     return this;
+//   },
+// };
+
+// ladder.up().up().down().showStep().down().showStep();
+
+// const price = 99.555;
+
+// const roundedPrice = price.toFixed(0);
+
+// console.log(roundedPrice);
+
+// console.log((5.123).toFixed(2));
+
+// const num = 100.055;
+
+// console.log(num.toPrecision(4));
+
+// console.log(num.toFixed(4));
+
+// const arr = [
+//   "Hi",
+//   3,
+//   true,
+//   { name: "Alex" },
+//   () => console.log("Haaha"),
+//   [true, true, false],
+// ];
+
+// console.log(arr[1]);
+
+// console.log(arr[3].name);
+
+// arr[4]();
+// console.log(arr[5][1]);
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// console.log(matrix[1][0]);
+
+// const arr = ["Hi", 3, true];
+
+// console.log(arr);
+
+// arr[0] = "by";
+// arr[1] = 200;
+// arr[2] = false;
+// arr[3] = 1111;
+// arr[5] = true;
+// console.log(arr);
+// console.log(arr[arr.length - 1]);
+// console.log(arr.at(-1));
+
+// const letters = ["A", "B", "C"];
+
+// console.log(letters);
+
+// letters.push("D", "E");
+// letters.shift();
+
+// console.log(letters);
+
+// const arr = ["Hi", 3, true];
+
+// console.log(arr.toString());
+// console.log(arr.join());
+
+// const arr1 = ["A", "B", "C"];
+// const arr2 = arr1.slice(-1);
+
+// // arr2[0] = "_";
+
+// console.log(arr1);
+// console.log(arr2);
+
+// const arr1 = ["A", "B"];
+// const arr2 = ["C", "D"];
+// const arr3 = ["E", "F"];
+
+// // const totalArr = [...arr1, ...arr2];
+
+// const totalArr = arr1.concat(arr2, arr3);
+
+// console.log(totalArr);
+
+// const arr1 = ["A", "B", "C", []];
+// const arr2 = ["A", "B", "C", []];
+
+// const areArrayEqual = (array1, array2) => {
+//   if (array1.length !== array2.length) {
+//     return false;
+//   }
+
+//   for (let i = 0; i < array1.length; i++) {
+//     const value1 = array1[i];
+//     const value2 = array2[i];
+
+//     const areValusesArrays = Array.isArray(value1) && Array.isArray(value2);
+//     if (areValusesArrays) {
+//       if (!areArrayEqual(value1, value2)) {
+//         return false;
+//       } else {
+//         continue;
+//       }
+//     }
+//     if (value1 !== value2) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// console.log(areArrayEqual(arr1, arr2));
+
+// const letters = ["A", "B", "C", "D", "E", "F"];
+
+// letters.forEach((letter) => {
+//   console.log(letter);
+// });
+
+// const prices = [100, 200, 300, 444, 500, 444, 1244];
+
+// console.log(prices.indexOf(444, 4));
+
+// const users = [
+//   { name: "Alex", age: 21 },
+//   { name: "Max", age: 12 },
+//   { name: "Egor", age: 32 },
+// ];
+
+// console.log(users.filter((user) => user.name === "Egor"));
+
+// console.log(users.every((user) => user.age >= 18));
+
+// console.log(users.some((user) => user.name === "Egor"));
+
+// console.log(
+//   users.findIndex((user) => {
+//     if (user.name === "Egor") {
+//       return true;
+//     }
+//   })
+// );
+
+// console.log(users.findIndex((user) => user.name === "Egor"));
+
+// const prices = [100, 200, 300, 444, 500, 444, 1244];
+
+// console.log(prices.includes(500));
+
+// const users = [
+//   { name: "Alex", age: 20, city: "Moscow" },
+//   { name: "Max", age: 10, city: "Moscow" },
+//   { name: "Egor", age: 30, city: "SPB" },
+// ];
+
+// const filterUser = users.filter(({ city, age }) => {
+//   return city === "Moscow" && age < 20;
+// });
+
+// console.log(filterUser);
+
+// const userFormatted = users.map((user) => {
+//   return `${user.name}, ${user.age}, ${user.city}`;
+// });
+
+// console.log(userFormatted);
+
+// let ageSum = 0;
+
+// for (let i = 0; i < users.length; i++) {
+//   ageSum += users[i].age;
+// }
+
+// console.log(ageSum / users.length);
+
+// const ageSum = users.reduce((sum, { age }) => sum + age, 0);
+
+// console.log(ageSum / users.length);
+
+// const reversedUser = users.reverse(); // мутирует изначальный обьект
+// const reversedUser1 = [...users].reverse(); // мутирует изначальный обьект
+
+// const numbers = [9, 4, 444, 4];
+// const sortedNumbers = [...numbers].sort((a, b) => a - b);
+
+// console.log(sortedNumbers);
